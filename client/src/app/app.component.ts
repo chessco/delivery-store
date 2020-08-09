a384d7a9787e99313845f7a109abc1d56cb40811
chessco <chesssco@gmail.com>
2020-08-08 19:24:05 -0700

    Initial Delivery Ecosystem

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from './shared/models/pagination';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Skinet';
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
+
